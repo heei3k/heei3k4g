3f78d59c9f5578c50ef5f01f151ade89be263f36
李华 <heei3k@hotmail.com>
2024-07-27 21:04:03 +0800

    v1.1.6.727 增加leetcode代码
    Signed-off-by: 李华 <heei3k@hotmail.com>

--- leetcode/leetcode002.py
+++ leetcode/leetcode002.py
@@ -39,11 +39,92 @@
 """
 
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class SingleLinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add_node(self, node: ListNode):
+        self.length += 1
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+
+
+def printNodeAsList(node):
+    result = []
+    while node is not None:
+        result.append(node.val)
+        node = node.next
+    print(result)
+
+
+def transferListAsSingleLinkedList(lst):
+    result = SingleLinkedList()
+    for val in lst:
+        node = ListNode(val)
+        result.add_node(node)
+    return result
+
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        pass
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum_ = carry + x + y
+            carry = sum_ // 10
+            curr.next = ListNode(sum_ % 10)
+            curr = curr.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry > 0:
+            curr.next = ListNode(carry)
+        return dummy.next
+
+
+if __name__ == '__main__':
+    # node1=ListNode(2)
+    # node2=ListNode(4)
+    # node3=ListNode(3)
+    # sl=SingleLinkedList()
+    # sl.add_node(node1)
+    # sl.add_node(node2)
+    # sl.add_node(node3)
+    #
+    # node4=ListNode(5)
+    # node5=ListNode(6)
+    # node6=ListNode(4)
+    # sl2=SingleLinkedList()
+    # sl2.add_node(node4)
+    # sl2.add_node(node5)
+    # sl2.add_node(node6)
+
+    l1 = [9, 9, 9, 9, 9, 9]
+    l2 = [9, 9, 9, 9]
+    sl = transferListAsSingleLinkedList(l1)
+    sl2 = transferListAsSingleLinkedList(l2)
+
+    solution = Solution()
+    result = solution.addTwoNumbers(sl.head, sl2.head)
+    printNodeAsList(result)
